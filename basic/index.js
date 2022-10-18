d241ef33b187f106866f49e3a12ee8e6a63d4f8b
myesn <myesn@foxmail.com>
2022-10-18 21:15:27 +0800

    feat: 新增 basic/b 包
    1. b 包依赖 a 包
    2. 顶级 basic 依赖 b 包
    3. 顶级 basic > index.js 中输出 b 包中的函数结果
    4. 安装(pnpm install)

--- basic/index.js
+++ basic/index.js
@@ -1,6 +1,8 @@
 const dayjs = require('dayjs');
 // 引用嵌套子包后，需要在项目根目录中执行 pnpm install 方可使用
 const a = require('a');
+const b = require('b');
 
 console.log(dayjs().format());
 console.log(`From package a: ${a.getMessage()}`);
+console.log(`From package b: ${b.getMessage()}`);
